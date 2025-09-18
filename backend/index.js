86449e878ee6f0c9bd25b6391ef69b9aa8aec056
anshika <vanshika960574@gamil.com>
2025-09-18 22:55:12 +0530

    project skeleton: backend

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use((req , res) =>{
+    console.log("server is working");
+    res.send("server is listening")
+})
+app.listen(port , () =>{
+    console.log(`✅ Server running on http://localhost:${port}`);
+
+});
+
