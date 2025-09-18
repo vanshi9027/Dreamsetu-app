56aeae981e6737cb890aa44f1250614179adeebf
anshika <vanshika960574@gamil.com>
2025-09-18 23:46:55 +0530

    new update

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+
+
+
 app.use((req , res) =>{
     console.log("server is working");
-    res.send("server is listening")
+    res.send("server is listening");
 })
 app.listen(port , () =>{
     console.log(`✅ Server running on http://localhost:${port}`);
